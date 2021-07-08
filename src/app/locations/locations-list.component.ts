77447084cc8cbd73e95a717001e648d1e9656a90
JLucas022 <joao.thenaisie.lucas@gmail.com>
2021-07-09 00:00:33 +0100

    Dependency injection and OnInit lifecycle hook

--- src/app/locations/locations-list.component.ts
+++ src/app/locations/locations-list.component.ts
@@ -1,59 +1,23 @@
+import { OnInit } from "@angular/core";
 import { Component } from "@angular/core";
 import { ILocation } from "./location";
+import { LocationService } from "./locations.service";
 
+// Component Decorator
 @Component({
-    selector: 'locations',
+    selector: 'locations',  // directive
     templateUrl: './locations-list.component.html',
     styleUrls: ['./locations-list.component.css']
 })
-export class LocationsListComponent {
-    locations: ILocation[] = [
-        {
-        name: "Caleidoscópio",
-        disp: true,
-        calls: false,
-        info: [
-          {
-            schedule: "-",
-            address: "-",
-            site: "-",
-          },
-        ],
-      },
-      {
-        name: "Biblioteca Galveias",
-        disp: true,
-        calls: false,
-        info: [
-          {
-            schedule: "-",
-            address: "-",
-            site: "-",
-          },
-        ],
-      },
-      {
-        name: "Biblioteca Coruchéus",
-        disp: true,
-        calls: false,
-        info: [
-          {
-            schedule: "-",
-            address: "-",
-            site: "-",
-          },
-        ],
-      },
-      {
-        name: "Biblioteca Nacional",
-        disp: false,
-        calls: false,
-        info: [
-          {
-            schedule: "-",
-            address: "-",
-            site: "-",
-          },
-        ],
-      }]
+// Class
+export class LocationsListComponent implements OnInit {
+    locations: ILocation[] = [];
+
+    // dependency injection
+    constructor(private locationService: LocationService) {}
+
+    // OnInit lifecycle hook
+    ngOnInit(): void {
+        this.locations = this.locationService.getLocations();
+    }
 }
