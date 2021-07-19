b2dfbadcf79c01a076d4d1c1945d4db4db449ec0
JLucas022 <joao.thenaisie.lucas@gmail.com>
2021-07-19 12:13:47 +0100

    Location model created and migrated

--- backend/workplacesbackend/location/models.py
+++ backend/workplacesbackend/location/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# model for location
+
+class Location(models.Model):
+    name = models.CharField(max_length=80)
+    disp = models.BooleanField(null=True)
+    calls = models.BooleanField(null=True)
+    address = models.CharField(max_length=100)
+    site = models.CharField(max_length=65)
+    contacts = models.PositiveBigIntegerField()
