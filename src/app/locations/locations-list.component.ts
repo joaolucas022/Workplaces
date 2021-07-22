67ad07f2f10dc4c3d6e362cb7753c08536ee8586
JLucas022 <joao.thenaisie.lucas@gmail.com>
2021-07-22 23:09:25 +0100

    Add and remove notes functions built

--- src/app/locations/locations-list.component.ts
+++ src/app/locations/locations-list.component.ts
@@ -19,6 +19,7 @@ export class LocationsListComponent implements OnInit, OnDestroy {
   clickedIndex!: number;
   suggestClicked: boolean = true;
   lunchChecked: boolean = false;
+  noteCount: number = 1;
 
   // dependency injection
   constructor(private locationService: LocationService) {}
@@ -63,7 +64,73 @@ export class LocationsListComponent implements OnInit, OnDestroy {
     this.suggestClicked = false;
   }
 
+  // opens or closes extra schedule slot
   onLunchCheck(): void {
     this.lunchChecked = !this.lunchChecked;
+  }
+
+  // adds a new note
+  addNote(): void {
+    const plus = document.getElementById('plus-button');
+    const minus = document.getElementById('minus-button');
+    const newDiv = document.createElement('div');
+    const newTextarea = document.createElement('textarea');
+    const newLabel = document.createElement('label');
+
+    this.noteCount++;
+    newDiv.classList.add('col-3');
+    newDiv.setAttribute('id', `note${this.noteCount}-div`);
+
+    // newDiv.innerHTML = `
+    // <label for="note-input${this.noteCount}" class="form-label">
+    //   <small>Nota ${this.noteCount}</small>
+    // </label>
+    // <textarea class="form-control" id="note-input${this.noteCount}" rows="3"></textarea>
+    // `;
+
+    newTextarea.classList.add('form-control');
+    newTextarea.setAttribute('id', `note${this.noteCount}-input`);
+    newTextarea.setAttribute('rows', '3');
+    newLabel.classList.add('form-label');
+    newLabel.setAttribute('for', `note${this.noteCount}-input`);
+    newLabel.innerHTML = `<small>Nota ${this.noteCount}</small>`;
+    newDiv.appendChild(newLabel);
+    newDiv.appendChild(newTextarea);
+
+    if (plus && plus.parentNode) {
+      plus.parentNode.insertBefore(newDiv, plus);
+
+      // regulates the position of plus and minus buttons
+      if (this.noteCount % 4 === 0 && minus) {
+        plus.style.margin = '5px 0 0 12px';
+        minus.style.margin = '5px 0 0 5px';
+      } else if (this.noteCount % 4 !== 0 && minus) {
+        plus.style.margin = '112px 0 0 0';
+        minus.style.margin = '84px 0 0 -24px';
+      } else {
+        plus.style.margin = '112px 0 0 0';
+      }
+    }
+  }
+
+  removeNote(): void {
+    const plus = document.getElementById('plus-button');
+    const minus = document.getElementById('minus-button');
+    const lastDiv = document.getElementById(`note${this.noteCount}-div`);
+    if (lastDiv && plus) {
+      lastDiv.remove();
+      this.noteCount--;
+
+      // regulates the position of plus and minus buttons
+      if (this.noteCount === 1) {
+        plus.style.margin = '80px 0 0 0';
+      } else if (this.noteCount % 4 === 0 && minus) {
+        plus.style.margin = '5px 0 0 12px';
+        minus.style.margin = '5px 0 0 5px';
+      } else if (this.noteCount % 4 !== 0 && minus) {
+        plus.style.margin = '112px 0 0 0';
+        minus.style.margin = '84px 0 0 -24px';
+      }
+    }
   }
 }
