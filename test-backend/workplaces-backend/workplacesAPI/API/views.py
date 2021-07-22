0e62e77fbfc2f9f6a92c25202ec942ae41ddad2f
JLucas022 <joao.thenaisie.lucas@gmail.com>
2021-07-22 13:59:10 +0100

    Test folder deleted

--- test-backend/workplaces-backend/workplacesAPI/API/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from locations.models import Location
-from .serializers import LocationSerializer
-
-
-class LocationViewSet(ModelViewSet):
-    queryset = Location.objects.all()
-    serializer_class = LocationSerializer
