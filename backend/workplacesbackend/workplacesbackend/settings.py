ec414c75d1b65804c411f0420912b307852fb1c3
JLucas022 <joao.thenaisie.lucas@gmail.com>
2021-07-28 08:09:09 +0100

    Added files for production

--- backend/workplacesbackend/workplacesbackend/settings.py
+++ backend/workplacesbackend/workplacesbackend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-anb@%bk8^l8s40%2(u)7j0tsgjqb+bd5=q64dp1f3o5tm#v5wo'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["work-places.herokuapp.com"]
 
 
 # Application definition
@@ -45,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,8 +131,9 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# CORS_ORIGIN_WHITELIST = [
-#      'http://localhost:4200'
-# ]
+CORS_ORIGIN_WHITELIST = [
+     "https://work-places.herokuapp.com/"
+]
 
-CORS_ORIGIN_ALLOW_ALL = True
+# Activate Django-Heroku
+django_heroku.settings(locals())
